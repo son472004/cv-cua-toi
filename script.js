f2ca58365c3331e5dcb32edbfb1157758e3e9d7a
son472004 <sonpham2004123@gmail.com>
2024-07-26 13:45:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDetails(id) {
+    const details = document.getElementById(id);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
